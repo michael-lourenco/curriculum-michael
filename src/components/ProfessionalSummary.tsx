4eeba48f62155e8af96e9f1843d1a6d9ea3a1c69
Michael Lourenco <michael.lourenco@pixter.com.br>
2025-01-13 00:48:43 -0300

    update experience

--- src/components/ProfessionalSummary.tsx
+++ src/components/ProfessionalSummary.tsx
@@ -5,7 +5,7 @@ const ProfessionalSummary: React.FC = () => {
     <section className="mb-8">
       <h3 className="text-2xl font-semibold border-b pb-2">Resumo Profissional</h3>
       <p className="mt-4 text-gray-700">
-        Desenvolvedor Back end com 4 anos de experiência em empresas de médio e grande porte, especializado em 
+        Desenvolvedor Back end com mais de 20 anos de experiência, atuando em empresas de médio e grande porte, especializado em 
         desenvolvimento e otimização de sistemas, gerenciamento de bancos de dados e melhoria de fluxos de negócios. 
         Atuo diariamente com tecnologias como Node.js, Python, AWS e GCP, e possuo experiência sólida em metodologias 
         ágeis e comunicação com stakeholders. Apaixonado por melhorar a eficiência dos sistemas e automatizar processos.
