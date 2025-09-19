0808f17f754285eaa47d14f224c4f463315ac3f5
Michael Lourenco <michael.lourenco@pixter.com.br>
2025-09-19 20:44:52 -0300

    novo chat

--- src/components/ChatbotScript.tsx
+++ src/components/ChatbotScript.tsx
@@ -16,6 +16,21 @@ export default function ChatbotScript() {
         data-id="9938612681"
         src="https://chatling.ai/js/embed.js"
       />
+
+      <Script
+        id="chatbot-config"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `window.chtlConfig = { chatbotId: "6835644195" };`,
+        }}
+      />
+      <Script
+        id="chatbot-script"
+        strategy="afterInteractive"
+        data-id="6835644195"
+        src="https://chatling.ai/js/embed.js"
+      />
     </>
   );
 } 
+
